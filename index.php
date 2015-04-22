6d1a2a1dada53ae94e7caf59f7f47ca2d13e9793
Cade Guevara <cadeguevara@yahoo.com>
2015-04-22 11:54:59 -0700

    finished milestone 2, created the index.php file

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cade's todo list2</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
