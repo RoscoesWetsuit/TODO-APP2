fa7664a15263b3fefa4d1b7ef7fc74f1cc2822f5
Cade Guevara <cadeguevara@yahoo.com>
2015-04-22 21:45:39 -0700

    finished milestone 12, added the logic behind showing our tasks

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,13 +1,23 @@
 <?php
- $task = strip_tags($_POST['task']);
- $date = date('Y-m-d');
- $time = date('H:i:s');
+	$task = strip_tags($_POST['task']);
+	$date = date('Y-m-d');
+	$time = date('H:i:s');
 
- includes('connect.php');
+	includes('connect.php');
 
- $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
- $mysqli ->query("INSERT INTO tasks VALUES ('', 'tasks', '$date', '$time')");
+	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	$mysqli ->query("INSERT INTO tasks VALUES ('', 'tasks', '$date', '$time')");
  
- $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
+	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 
+	if($result = $myslqi->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+ 			$task_id = $row['id'];
+ 			$task_name = $row['task'];
+		}
+	}
+
+	$myslqi->close();
+
+	echo '<li><span>',$task_name.'<span/><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
 ?>
