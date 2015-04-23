69278503e4611680c7034a767caed393a088150b
Cade Guevara <cadeguevara@yahoo.com>
2015-04-22 22:51:27 -0700

    sorta fixed some errors

--- includes/add-task.php
+++ includes/add-task.php
@@ -3,21 +3,21 @@
 	$date = date('Y-m-d');
 	$time = date('H:i:s');
 
-	includes('connect.php');
+	include('connect.php');
 
 	$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 	$mysqli ->query("INSERT INTO tasks VALUES ('', 'tasks', '$date', '$time')");
  
 	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 
-	if($result = $myslqi->query($query)) {
+	if($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
  			$task_id = $row['id'];
  			$task_name = $row['task'];
 		}
 	}
 
-	$myslqi->close();
+	$mysqli->close();
 
-	echo '<li><span>',$task_name.'<span/><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
+	echo '<li><span>'.$task_name.'<span/><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
 ?>
