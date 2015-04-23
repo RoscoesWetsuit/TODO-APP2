4fa6c889b9bd8c2ce42ee08f7b7541e08f0dba5a
Cade Guevara <cadeguevara@yahoo.com>
2015-04-22 20:43:08 -0700

    finished milestone 5, created includes folder

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 //$mysql->connect_error then we want it to die and have this message
 if($mysqli->connect_error) {
 	die('connect errror (' . $mysqli->connect_errno . ')'
